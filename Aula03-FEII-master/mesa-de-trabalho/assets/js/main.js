7206fa3bdec6043add12f60949a6dac180d4bf22
johnnyrps <johnnyrps@gmail.com>
2022-02-23 21:03:06 -0300

    front2-aula3

---
+++ Aula03-FEII-master/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,27 @@
+var adicionandoX = document.getElementById("linha-2_coluna-2");
+
+adicionandoX.innerText = "X";
+
+var adicionandoO = document.getElementById("linha-3_coluna-3");
+
+adicionandoO.innerText="O";
+
+adicionandoX = document.getElementById("linha-3_coluna-2");
+
+adicionandoX.innerText="X";
+
+adicionandoO = document.getElementById("linha-1_coluna-2");
+
+adicionandoO.innerText="O";
+
+document.getElementById("linha-1_coluna-1").innerText = "X"
+
+document.getElementById("linha-1_coluna-3").innerText = "O";
+
+adicionandoX = document.getElementById("linha-2_coluna-3");
+
+adicionandoX.innerText="X";
+
+document.getElementById("linha-2_coluna-1").innerText = "O";
+
+document.getElementById("linha-3_coluna-1").innerText = "X";
